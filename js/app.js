effd55ae45ef5ae606e0d8448468e58f77f5667e
John Doe <hamedhasan422421@gmail.com>
2022-02-17 18:17:01 +0300

    update calculation

--- js/app.js
+++ js/app.js
@@ -1,106 +1,137 @@
- 
+// getting input html elements
+let incomeField = document.querySelector('#income');
+let foodField = document.querySelector('#food');
+let rentField = document.querySelector('#rent');
+let clothesField = document.querySelector('#clothes');
 
+let btnCalculate = document.querySelector('#calculate');
+let displayCost = document.querySelector('#total-expenses');
+let displayBalance = document.querySelector('#balance');
+let balanceError = document.querySelector('#balanceError');
 
-document.getElementById('calculate').addEventListener('click', function () {
-      updateCalculate('income')
-})
+let savingField = document.querySelector('#save');
+let btnSave = document.querySelector('#btn-save');
+let displaySaving = document.querySelector('#saving-value');
+let displayRemaining = document.querySelector('#remaining-value');
 
-function updateCalculate(inputId){
-    const inputTotal = document.getElementById(inputId).value
-    const food = document.getElementById('food').value
-    const rent = document.getElementById('rent').value
-    const clothes = document.getElementById('clothes').value
+//clear input field
+function clearInput() {
+    incomeField.value = foodField.value = rentField.value = clothesField.value = displayCost.innerHTML = displayBalance.innerHTML = savingField.value = displaySaving.innerHTML = displayRemaining.innerHTML = '';
+    incomeField.setAttribute('placeholder', '0');
+    foodField.setAttribute('placeholder', '0');
+    rentField.setAttribute('placeholder', '0');
+    clothesField.setAttribute('placeholder', '0');
+    savingField.setAttribute('placeholder', '0');
+    displayCost.innerHTML = '0';
+    displayBalance.innerHTML = '0';
+    displaySaving.innerHTML = '0';
+    displayRemaining.innerHTML = '0';
+}
+clearInput();
 
-    const total = parseFloat(food) + parseFloat(rent) + parseFloat(clothes)
-    const totalExpenses = document.getElementById('total-expenses')
-    const totalExpensesFloat = parseFloat(totalExpenses.innerText)
-    totalExpenses.innerText = total
-    let minusValue = inputTotal - total
- 
-    const balance = document.getElementById('balance')
-    const inputCostFloat = parseFloat(balance)
-    balance.innerText = Math.abs(minusValue)
-    
-    
-        const success = document.getElementById('success')
-        const error = document.getElementById('error')
-        if(inputTotal == '' || inputTotal < 0 || food == '' || food < 0 || rent == '' || rent < 0 || clothes == '' || clothes < 0){
-            console.log('please valid number')
-            error.style.display = 'block'
-        }else{
-            success.style.display = 'block'
-            error.style.display = 'none'
-        }
-
-
+//text to number
+function stringToNumber(str) {
+    let num = parseFloat(str);
+    return num;
 }
 
-
-document.getElementById('save-btn').addEventListener('click', function(){
-        saveAmount('income')
-    
-})
-
-
-function saveAmount(inputId){
-    const income = document.getElementById(inputId).value
-    const incomeTotal = parseFloat(income)
-
-    const saveInput = document.getElementById('save-input').value
-    const saveInputFloat = parseFloat(saveInput)
-
-    const savingCalculate = (incomeTotal / 100) * saveInputFloat;
-    
-    const savingAmount = document.getElementById('saving-amount')
-    savingAmount.innerText = savingCalculate.toFixed()
-
-    const balance = document.getElementById('balance').innerText
-    const inputCostFloat = parseFloat(balance)
-    const remainingTotal = inputCostFloat - savingCalculate
-
-    const remainingAmount = document.getElementById('remaining-amount')
-    remainingAmount.innerText = remainingTotal.toFixed()
-
-    const success = document.getElementById('successMsg')
-        const error = document.getElementById('errorMsg')
-        if(saveInput == '' || saveInput < 0){
-            console.log('please valid number')
-            error.style.display = 'block'
-            success.style.display = 'none'
-        }else{
-            success.style.display = 'block'
-            error.style.display = 'none'
-        }
+//display
+function display(where, amount) {
+    where.innerHTML = amount
 }
-    
 
+//Input Field value
+let income = foodCost = rentCost = clothCost = totalCost = balance = saving = 0;
+balance = income;
 
+//check negative positive and produce error
+function validationError(field, cost) {
+    if (cost >= 0) {
+        field.setCustomValidity("");
+    } else if (cost < 0) {
+        field.setCustomValidity("Can't be Negative");
+    } else {
+        field.setCustomValidity("The field must be a number");
+    }
+}
 
+//updateInput
+function updateInput() {
+    if (incomeField.value == '') {
+        income = 0;
+    } else {
+        income = stringToNumber(incomeField.value);
+    }
 
+    if (foodField.value == '') {
+        foodCost = 0;
+    } else {
+        foodCost = stringToNumber(foodField.value);
+        validationError(foodField, foodCost);
+    }
 
+    if (rentField.value == '') {
+        rentCost = 0;
+    } else {
+        rentCost = stringToNumber(rentField.value);
+        validationError(rentField, rentCost);
+    }
 
+    if (clothesField.value == '') {
+        clothCost = 0;
+    } else {
+        clothCost = stringToNumber(clothesField.value);
+        validationError(clothesField, clothCost);
+    }
+    if (savingField.value == '') {
+        saving = 0;
+    } else {
+        saving = stringToNumber(savingField.value);
+        validationError(savingField, saving);
+    }
+}
+//set default value;
+updateInput();
+//Calculate & Display using calculate button
+btnCalculate.addEventListener('click', function () {
+    updateInput();
+    if (!Number.isNaN(income)) {
+        if (income >= 0) {
+            totalCost = foodCost + rentCost + clothCost;
+            balance = income - totalCost;
+            display(displayCost, totalCost);
 
+            if (income >= totalCost) {
+                display(displayBalance, balance);
+                displayBalance.style.color = "black";
+            } else {
+                display(displayBalance, `${balance}, Error: The budget is less than your expenses.`);
+                displayBalance.style.color = "#ff4f5a";
+            }
+            incomeField.setCustomValidity('');
+        } else {
+            incomeField.setCustomValidity("Can't be Negative No.");
+        }
+    } else if (Number.isNaN(income)) {
+        incomeField.setCustomValidity("The field must be a number");
+    }
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Seving Section Work Start From Here 
+btnSave.addEventListener('click', function () {
+    updateInput();
+    balance = income - totalCost;
+    seving = saving / 100;
+    let sevingAmmount = income * seving;
+    display(displaySaving, sevingAmmount);
+    let remainingBalance = balance - sevingAmmount;
+    if (remainingBalance >= 0) {
+        display(displayRemaining, remainingBalance);
+        displayRemaining.style.color = "black";
+        savingField.setCustomValidity('');
+    } else {
+        display(displayRemaining, `${remainingBalance} Error: Please lower the savings rate.`);
+        displayRemaining.style.color = "#ff4f5a";
+        savingField.setCustomValidity('Please lower the savings rate.');
+    }
+});
